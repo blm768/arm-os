eccc52f98a768f1bc432ea61d4a31077d7eda2eb
Ben Merritt <blm768@gmail.com>
2013-08-08 10:47:04 -0700

    Fixed console bug

--- kernel/platform/qemu-integrator-cp/console.c
+++ kernel/platform/qemu-integrator-cp/console.c
@@ -1,8 +1,11 @@
 #include "console.h"
 
 void write(char* str) {
-	for(char c = *str; c != '\0'; ++str) {
-		write_char(*str);
+	char c = *str;
+	while(c != '\0') {
+		write_char(c);
+		++str;
+		c = *str;
 	}
 }
 
